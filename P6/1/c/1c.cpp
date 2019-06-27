4bd374222dc35402dd7eba3d92d2b28d3cbe5311
LFRusso <lf.santos@usp.br>
2019-06-27 16:52:43 -0300

    1c

--- P6/1/c/1c.cpp
+++ P6/1/c/1c.cpp
@@ -3,33 +3,44 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
-#include <omp.h>
 #define k 100
 using namespace std;
 
 int main(){
-   int S_in{0}, S_total{0};
-   ofstream data;
+   int S_in{0};
    srand(42);
-   double x, y;
+   double x, y, med;
    double E{1};
-   data.open("data");
-   int i = 0;
+   int count{0};
+   vector<double> Pi_Values;
+   Pi_Values.reserve(100);
 
-   #pragma omp parallel private(x, y, i) shared(S_in, S_total) num_threads(4)
-   {
-      #pragma omp for
-      for (i = 0; i <= 10000000; i ++){
-         x = (double(rand() % int(pow(10, 5)))/pow(10, 5))*2 - 1;
-         y = (double(rand() % int(pow(10, 5)))/pow(10, 5))*2 - 1;
-         if(pow(x,2)+pow(y,2) <= 1){
-            ++S_in;
+      while(E >= pow(10, -4)){
+         ++count;
+         med = 0;
+         E = 1;
+         for(int i = 0; i<= 100; ++i){
+            S_in = 0;
+            for(int j = 0; j <= pow(2, count); j++){
+               x = 2*double(rand() % int(pow(10, 5)))/pow(10, 5) - 1;
+               y = 2*double(rand() % int(pow(10, 5)))/pow(10, 5) - 1;
+               if(pow(x, 2) + pow(y, 2) <= 1){
+                  S_in++;
+               }
+            }
+            Pi_Values[i] = 4*double(S_in)/pow(2, count);
          }
-         ++S_total;
+
+         for(int i =0; i <= 100; i++){
+            med += Pi_Values[i];
+         }
+         med /= 100;
+         for(int i; i <= 100; i++){
+            E +=pow(Pi_Values[i] - med, 2);
+         }
+         E = sqrt(E);
+         cout << count << endl;
       }
-   }
-   cout << (double(S_in)/double(S_total))*4 << endl;
-   data.close();
-   //system("python3 graph.py");
+
    return 0;
 }
